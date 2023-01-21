216f8ed788825eaf909bc75ea9ab939fabae850e
fikreanteneh <fikre.anteneh01@gmail.com>
2023-01-21 13:15:42 +0300

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let player = ["X", "O"]
 let position = [0]
 let played = ["","","","","","","","",""]
-let score = [0, 0]
+let score = [0, 0, 0]
 let ended = false
 
 const buttons = document.querySelectorAll(".box")
 const remind = document.querySelector(".remind")
 const playerX = document.querySelector(".playerX")
 const playerO = document.querySelector(".playerO")
+const tie = document.querySelector(".tie")
 const restart = document.querySelector(".restart")
-
 
 function marker(element, position, x) {
     if (ended){ 
@@ -31,7 +31,10 @@ function gameEnd(){
     position = [0]
     played = ["","","","","","","","",""]
     buttons.forEach(element =>{
-        element.innerHTML = ""})
+        element.className = "box"
+        element.innerHTML = ""
+
+    })
     remind.innerHTML = ""
 }
 
@@ -44,41 +47,72 @@ function match(board){
         return false
     }
 }
-function gameWon(win){
+function gameWon(win, winPos){
     remind.innerHTML = `Player ${player[win - 1]} won the game!!!`
     score[win-1] += 1
     playerX.innerHTML = score[0]
     playerO.innerHTML = score[1]
+    // let winPos = [buttons[indexes[0]], buttons[indexes[1]], buttons[indexes[2]]]
+    winPos.forEach( element =>{
+        element.classList.toggle("active")
+    })
     ended = true 
 
 }
 function winChecker(){
-    for(let i = 0; i <= 6; i+=3){
-       play = [played[i], played[i + 1 ], played[i + 2]]
-       let win = match(play)
-       if (win){
-        gameWon(win)
-        return
-    }
-    }
-    for(let i = 0; i < 3; i++){
-        play = [played[i + 0], played[i + 3], played[i + 6]]
-        win = match(play)
-        if (win){ 
-            gameWon(win)
-            return
+    let checkPos = [
+        [[played[0], played[1],played[2]], [buttons[0], buttons[1],buttons[2]]],
+        [[played[3], played[4],played[5]], [buttons[3], buttons[4],buttons[5]]],
+        [[played[6], played[7],played[8]], [buttons[6], buttons[7],buttons[8]]],
+        [[played[0], played[3],played[6]], [buttons[0], buttons[3],buttons[6]]],
+        [[played[1], played[4],played[7]], [buttons[1], buttons[4],buttons[7]]],
+        [[played[2], played[5],played[8]], [buttons[2], buttons[5],buttons[8]]],
+        [[played[0], played[4],played[8]], [buttons[0], buttons[4],buttons[8]]],
+        [[played[2], played[4],played[6]], [buttons[2], buttons[4],buttons[6]]]
+        ]
+    checkPos.every(element =>{
+
+        let play = element[0]
+        let boxes = element[1]
+        let win = match(play)
+        if (win){
+            gameWon(win, boxes)
+            return false
         }
-     }
-    diagonal = [[played[0], played[4], played[8]], [played[2], played[4], played[6]]]
-    diagonal.forEach(element => {
-        win = match(element)
-        if (win){ 
-            gameWon(win)
-            return
-        }
+        return true
     })
+    
+    // for(let i = 0; i <= 6; i+=3){
+    //    play = [played[i], played[i + 1 ], played[i + 2]]
+    //    let win = match(play)
+    //    if (win){
+    //     gameWon(win, [i, i+1, i+2])
+    //     return
+    // }
+    // }
+    // for(let i = 0; i < 3; i++){
+    //     play = [played[i], played[i + 3], played[i + 6]]
+    //     win = match(play)
+    //     if (win){ 
+    //         gameWon(win, [i, i+3, i+6])
+    //         return
+    //     }
+    //  }
+    // for (let i = 0; i < 2; i++){
+    //     play = [played[i*2], played[4], played[8-(i*2)]]
+    //     win = match(play)
+    //     if (win){ 
+    //         gameWon(win, [i*2, 4, 8 - (i*2)])
+    //         return
+    //     }
+    // }
     if (!played.includes("") && remind.innerHTML==""){
         remind.innerHTML = "No Body Won the game!!!"
+        buttons.forEach(element =>{
+            element.classList.toggle("tied")
+        })
+        score[2] += 1
+        tie.innerHTML = score[2]
         ended = true
         return
     }
@@ -90,8 +124,12 @@ buttons.forEach( (element,x) => {
 
 restart.addEventListener("click", e=>{
     let end = setTimeout(gameEnd, 1500)
-    score = [0, 0]
+    score = [0, 0, 0]
     playerO.innerHTML = 0
     playerX.innerHTML = 0
+    tie.innerHTML = 0
 })
 
+
+// diagonal = [[played[0], played[4], played[8]], [played[2], played[4], played[6]]]
+    // diagonal.forEach( (element, index) => {
